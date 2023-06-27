420e298d81e29a71f34ffc623a915839301d18b7
hromadalubo <112410335+hromadalubo@users.noreply.github.com>
2023-06-27 14:29:43 +0200

    Initial Commit

---
+++ Pokemons/PokemonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonsApp.swift
+//  Pokemons
+//
+//  Created by Lubos Hromada on 27/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
